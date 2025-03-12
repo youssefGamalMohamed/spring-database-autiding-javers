82ad406f7d9450639bb4efa4f989b44d13e60933
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-12 15:38:42 +0200

    feat(Post): Added Entity for It

---
+++ src/main/java/com/youssef/gamal/javers_auditing/entities/Post.java
@@ -0,0 +1,20 @@
+package com.youssef.gamal.javers_auditing.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@Table("posts")
+public class Post {
+
+    @Id
+    private Long id;
+
+    private String title;
+    private String content;
+    
+}
