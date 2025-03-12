94d80b28a8af01561541feabb2ff438a121d48a9
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-12 13:23:44 +0200

    Adding Initialized Project Code

---
+++ src/main/java/com/youssef/gamal/javers_auditing/JaversAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.youssef.gamal.javers_auditing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaversAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaversAuditingApplication.class, args);
+	}
+
+}
