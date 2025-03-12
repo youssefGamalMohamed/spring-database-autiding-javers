0820a59a79dc57ee9defdebac2534ae26e324bf3
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-13 00:15:19 +0200

    feat(PostRepo): Support Paging ans Sorting and List All

--- src/main/java/com/youssef/gamal/javers_auditing/repos/PostRepo.java
+++ src/main/java/com/youssef/gamal/javers_auditing/repos/PostRepo.java
@@ -1,9 +1,11 @@
 package com.youssef.gamal.javers_auditing.repos;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.youssef.gamal.javers_auditing.entities.Post;
 
-public interface PostRepo extends CrudRepository<Post,Long> {
+public interface PostRepo extends ListCrudRepository<Post,Long> 
+    , PagingAndSortingRepository<Post,Long> {
 
 }
