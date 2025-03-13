f1701f2cd80db31ea78ea84bbb6a6fe4da4fd85b
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-13 04:16:20 +0200

    feat(AuthorProvider): Allow Javer to Get the Current Auditor

---
+++ src/main/java/com/youssef/gamal/javers_auditing/auditing/SimpleAuthorProvider.java
@@ -0,0 +1,12 @@
+// package com.youssef.gamal.javers_auditing.auditing;
+
+// import org.javers.spring.auditable.AuthorProvider;
+
+// public class SimpleAuthorProvider implements AuthorProvider {
+
+//     @Override
+//     public String provide() {
+//         return "Youssef Gamal";
+//     }
+
+// }
