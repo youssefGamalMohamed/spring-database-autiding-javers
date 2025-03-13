42062b33762756022cd4668f8e663323eeb22cc8
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-13 04:16:58 +0200

    feat(Post): Remove Spring Data JDBC Annotantions and Use Spring Data JPA

--- src/main/java/com/youssef/gamal/javers_auditing/entities/Post.java
+++ src/main/java/com/youssef/gamal/javers_auditing/entities/Post.java
@@ -1,17 +1,26 @@
 package com.youssef.gamal.javers_auditing.entities;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@Table("POSTS")
+@Table(name ="POSTS")      
+@Entity         
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
