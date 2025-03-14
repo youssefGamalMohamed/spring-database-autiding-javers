7b65ecd2c1ff2b906dc382453f40f1a3840b82a5
Youssef.Gamal <youssef.gamal@intercom.com.eg>
2025-03-14 14:34:26 +0200

    feat(PostDto): Appear With Name Post Into Swagger

--- src/main/java/com/youssef/gamal/javers_auditing/dtos/PostDto.java
+++ src/main/java/com/youssef/gamal/javers_auditing/dtos/PostDto.java
@@ -1,7 +1,9 @@
 package com.youssef.gamal.javers_auditing.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(name = "Post")
 @Builder
 public record PostDto(Long id, String title ,String content) {
 
